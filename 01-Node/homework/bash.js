eb666953309b9a7366d34b2d6cc717a96261e625
Agustín Ceballos <agustin.97.ceballos@gmail.com>
2022-11-01 18:41:38 -0300

    hw node

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -1,1 +1,26 @@
-console.log(process)
+const command = require('./commands/index');
+
+//{date: function(done),{done(Date()},
+//pdw:function(done),{done(process.cwd())}
+
+const done = function(output) {
+    process.stdout.write(output)
+    process.stdout.write('\nprompt > ')
+}
+
+// Output un prompt
+process.stdout.write('prompt > ');
+// El evento stdin 'data' se dispara cuando el user escribe una línea
+process.stdin.on('data', function (data) {
+    var arg = data.toString().trim().split(" "); //echo hola cohorte 31b === [echo hola cohorte 31b]
+    var cmd = arg.shift()  // arg = [echo]
+    //process.stdout.write(cmd);
+
+    if(command.hasOwnProperty(cmd)){
+        command[cmd](arg, done)
+    }
+    else{
+        process.stdout.write(`${cmd} not found`)
+        process.stdout.write('\nprompt > ')
+    }
+});
--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,5 +1,62 @@
+// if(cmd === 'date') {
+//     process.stdout.write(Date());  
+// }
+// if(cmd === 'pwd') {
+//     process.stdout.write('\n' + process.cwd()) 
+// }
+// process.stdout.write('\nprompt > ');
+
+var fs = require('fs')
+//npm i request
+var request = require("request")
 
 
 module.exports = {
-
+    date: function(arg, done) {
+        done(Date())
+    },
+    pwd: function(arg, done) {
+        done(process.cwd())
+    },
+    //listado directorio/archivos
+    ls: function(arg, done) {
+        fs.readdir('.', 'utf-8', function(err, files) {
+            if(err) throw err
+            //console.log("soy files", files)
+            var lista = files.join("\n")
+            done(lista)
+        })
+    },
+    echo: function(arg, done) {
+        done(arg.join(" "))
+    },
+    cat: function(arg, done) {
+        fs.readFile(arg[0], 'utf-8', function(err, data) {
+            if(err) throw err;
+            //console.log("soy data", data)
+            done(data)
+        })
+    },
+    head: function(arg, done) {
+        fs.readFile(arg[0], 'utf-8', function(err, data) {
+            if(err) throw err;
+            const lineas = data.split("\n").slice(0,7).join("\n")
+            //console.log(lineas)
+            done(lineas)
+        })
+    },
+    tail: function(arg, done) {
+        fs.readFile(arg[0], 'utf-8', function(err, data) {
+            if(err) throw err;
+            const lineas = data.split("\n").slice(-3).join("\n")
+            //console.log(lineas)
+            done(lineas)
+        })
+    },
+    curl : function(arg, done) {
+        request(arg[0], function(err, body) {
+            if(err) throw err;
+            done(body)
+        })
+    }
 }
