a5669ced77fe211c0d5ea420591f5a5ef85e860a
Agustín Ceballos <agustin.97.ceballos@gmail.com>
2022-11-01 18:34:14 -0300

    f c

---
+++ 01-Node/demo/demo.js
@@ -0,0 +1,9 @@
+//1er numero = major
+//2do numero =Minor
+
+
+// v 5.1.0 --> salió primero
+// v 5.2.0 --> suma funcionalidades,es compatible
+// v 5.2.2 --> solucionar bugs
+
+// v 6.0.3 --> cambio de versión 
---
+++ 01-Node/demo/patrones/otro/saludo1.js
@@ -0,0 +1,5 @@
+module.exports = function() {
+	console.log('Hola mundo primer archivo saludo1');
+};
+
+// Esta es la forma que conociamos!
